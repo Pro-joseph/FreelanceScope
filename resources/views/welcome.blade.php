a4bbfa5e33e2252171ac750b30e20d75703c09c3
Joseph <jdirayoussef1997@gmail.com>
2026-07-29 16:10:58 +0100

    Fix typo in welcome page description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 <body class="bg-gray-50 dark:bg-gray-900 flex items-center justify-center min-h-screen">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-gray-800 dark:text-white mb-4">FreelanceScope</h1>
-        <p class="text-gray-600 dark:text-gray-400">Projects-aEstimation & Management</p>
+        <p class="text-gray-600 dark:text-gray-400">Projects-Estimation & Management</p>
     </div>
 </body>
 </html>
