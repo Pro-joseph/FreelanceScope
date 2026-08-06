9f1309c10969450f5f3bb9ddc7042b3047314aea
Joseph <jdirayoussef1997@gmail.com>
2026-08-06 19:06:26 +0100

    fix: add leading space to the FreelanceScope title for consistency

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="bg-gray-50 dark:bg-gray-900 flex items-center justify-center min-h-screen">
     <div class="text-center">
-        <h1 class="text-4xl font-bold text-gray-800 dark:text-white mb-4">FreelanceScope</h1>
+        <h1 class="text-4xl font-bold text-gray-800 dark:text-white mb-4"> FreelanceScope</h1>
         <p class="text-gray-600 dark:text-gray-400">Projects-Estimation & Management</p>
     </div>
 </body>
